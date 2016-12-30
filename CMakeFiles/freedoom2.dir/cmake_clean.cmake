ac72f6f78b17eefead6b71c57617707d254e8331
root <root@debianML>
2016-12-30 03:37:56 +0300

    simple health-gathering DQN

--- CMakeFiles/freedoom2.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/freedoom2"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/freedoom2.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
